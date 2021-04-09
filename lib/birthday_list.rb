bc70119aed0ac4217ad8abe535c892b8be0cfd6b
YohannTisserand <tisserand.yohann@gmail.com>
2021-04-09 13:55:01 +0100

    show method added

--- lib/birthday_list.rb
+++ lib/birthday_list.rb
@@ -1,1 +1,23 @@
+class Birthday
+
+  def initialize
+    @friends = []
+  end
+
+  def add(name, day)
+    @friends << { name: name, day: day }
+  end
+  
+  def track
+    @friends.each do |birth|
+      puts "#{birth[:name]} = #{birth[:day]}"
+    end
+  end
+
+  def show
+    @friends.each do |birth|
+      puts "Today's birthday: #{birth[:name]}. This friend has #{birth} years old."
+    end    
+  end
+end
 
--- spec/birthday_list_spec.rb
+++ spec/birthday_list_spec.rb
@@ -1,1 +1,33 @@
+require 'birthday_list'
 
+describe Birthday do
+  context '#add' do
+    it { is_expected.to respond_to(:add).with(2).argument }
+
+    it 'should add a birthday to the list' do
+      birthday = Birthday.new
+      expect(birthday.add('Yohann', '14 Octobre 1983')).to eq [{ name: 'Yohann', day: '14 Octobre 1983' }]
+    end
+  end
+
+  context '#track' do
+    it { is_expected.to respond_to(:track) }
+
+    it 'should track all the birthday at once' do
+      birthday = Birthday.new
+      birthday.add('Yohann', '14 Octobre 1983')
+      expect{ birthday.track }.to output.to_stdout
+    end
+  end
+
+  context '#show' do
+    it { is_expected.to respond_to(:show) }
+
+    it 'should show a particular birthday' do
+      birthday = Birthday.new
+      birthday.add('Yohann', '14 Octobre 1983')
+      birthday.add('Mogane', '16 Octobre 1983')
+      expect{ birthday.show }.to output.to_stdout
+    end
+  end
+end
